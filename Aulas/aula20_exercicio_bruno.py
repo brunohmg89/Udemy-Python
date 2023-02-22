56384c106463861fd6adc258573653fc6705cbd7
unknown <henrique.brunohmg@gmail.com>
2023-02-22 09:40:30 -0300

    feat :zap: aulas python

---
+++ Aulas/aula20_exercicio_bruno.py
@@ -0,0 +1,14 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+
+    print(f'primeiro valor "{primeiro_valor}" é maior que o segundo valor "{segundo_valor}"')
+
+elif primeiro_valor < segundo_valor:
+
+    print(f'primeiro valor "{primeiro_valor}" é menor que o segundo valor "{segundo_valor}"')
+
+else:
+
+    print('Os valores digitados são iguais')
