a0811f2b7b96bfb4e147297b80fad338bc918f9b
unknown <henrique.brunohmg@gmail.com>
2023-03-07 09:24:40 -0300

    feat :zap: aulas python 07-03

---
+++ Aulas/aula65_exercicio_s2.py
@@ -0,0 +1,42 @@
+"""
+Calculo do segundo dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF,
+MAIS O PRIMEIRO DIGITO,
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 11
+Ex.:  746.824.890-70 (7468248907)
+   11 10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
+   77 40 54 64 14 24 40 36  0 14
+Somar todos os resultados:
+77+40+54+64+14+24+40+36+0+14 = 363
+Multiplicar o resultado anterior por 10
+363 * 10 = 3630
+Obter o resto da divisão da conta anterior por 11
+3630 % 11 = 0
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+O segundo dígito do CPF é 0
+"""
+
+cpf = '74682489070'
+dez_digitos = cpf[:9] + '7'
+contador_regressivo_2 = 11
+resultado_digito_2 = 0
+
+for digito_2 in dez_digitos:
+    # print(digito_2, contador_regressivo_2) # mostra digito e contador
+    # print(digito_2 * contador_regressivo_2) # nesse momento cpf é uma string
+    # print(int(digito_2) * contador_regressivo_2) # valores das multiplicações
+    resultado_digito_2 += int(digito_2) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+
+# print((resultado_digito_2 * 10) % 11) # print do resultado do primeiro digito
+
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+# print(digito_2) print do segundo digito
